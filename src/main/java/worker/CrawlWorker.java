577c7e6231dea3801716d09ded110041f8f891e7
Somil Govani <somilgovani@gmail.com>
2019-04-23 23:20:31 -0400

    changing master

--- src/main/java/worker/CrawlWorker.java
+++ src/main/java/worker/CrawlWorker.java
@@ -42,7 +42,7 @@ public class CrawlWorker {
 	public static final double MAX_DOC_SIZE = 2 * 1e6;
 	public static boolean working;
 	static Logger log = LogManager.getLogger(CrawlWorker.class);
-	public static final String MASTER_URL = "http://ec2-54-225-59-102.compute-1.amazonaws.com";
+	public static final String MASTER_URL = "http://ec2-174-129-137-21.compute-1.amazonaws.com";
 	//public static final String MASTER_URL = "http://localhost";
 	public static final String MASTER_IP =  MASTER_URL + ":" + CrawlMaster.myPort;
 	public static final String MASTER_IP_MQ = "ec2-54-225-59-102.compute-1.amazonaws.com";
