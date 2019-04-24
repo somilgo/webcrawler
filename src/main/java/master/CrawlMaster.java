50708182b441042cfa264fda9f21fbfb7882668f
Somil Govani <somilgovani@gmail.com>
2019-04-24 19:53:36 -0400

    timing for testing

--- src/main/java/master/CrawlMaster.java
+++ src/main/java/master/CrawlMaster.java
@@ -208,12 +208,15 @@ public class CrawlMaster {
 
 	private static void outputURLs() {
 		while (true) {
-			int whileCount = 0;
-			log.info("LOOP");
 			while (urlCache.size() > 500 || urlThreadCount.get() > 30) {
-				if (whileCount % 2 == 0) log.info("tHREADS = " + urlThreads);
+				synchronized (urlThreads) {
+					log.info("tHREADS = " + urlThreads);
+				}
+
+				synchronized (urlCache) {
 				log.info("Sleeping because urlCache size=" + urlCache.size()
 						+ " and thread count = " + urlThreadCount.get());
+				}
 				try {
 					Thread.sleep(500);
 				} catch (InterruptedException e) {
