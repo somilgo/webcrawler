1f5c32bbc6ecabde4bb6e011e02ec5d30c84de70
Somil Govani <somilgovani@gmail.com>
2019-04-25 19:16:14 -0400

    timing for testing

--- src/main/java/master/CrawlMaster.java
+++ src/main/java/master/CrawlMaster.java
@@ -216,7 +216,7 @@ public class CrawlMaster {
 			int urlThreadCountCurr = urlThreadCount.get();
 			while (cacheSize > 500 ||  urlThreadCountCurr > 49) {
 				try {
-					Thread.sleep(500);
+					Thread.sleep(5000);
 					synchronized (urlCache) {
 						log.info("Slept because urlCache size=" + cacheSize
 								+ " and thread count = " + urlThreadCountCurr);
